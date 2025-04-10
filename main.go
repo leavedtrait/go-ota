eaf22056321c4c0cfb9f8c594f068d99eed5d4a2
leavedtrait <leavedtrait@gmail.com>
2025-04-10 22:38:33 +0300

    added  pgx

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 	"log"
+	"os"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/leavedtrait/go-ota/cmd/api"
+	"github.com/leavedtrait/go-ota/internal/db"
 	"github.com/leavedtrait/go-ota/internal/server"
 )
 
 func main() {
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	conn ,err := db.ConnectToDB() 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
@@ -21,6 +21,7 @@ func main() {
 	defer conn.Close(context.Background())
 
 	router := server.NewRouter(conn)
+	
 	srv := api.NewServer(":3000", router)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err.Error())

08b6169a785c1c2210bff4d8fcef37e7ae5b977a
leavedtrait <leavedtrait@gmail.com>
2025-04-10 23:11:03 +0300

    added pgx

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+
 	"github.com/jackc/pgx/v5"
 )
 
 func NewRouter(db *pgx.Conn) *http.ServeMux {
 	mux := http.DefaultServeMux
-	
-
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+	  fmt.Fprintf(w, "Hello, World!")
+	})
 	return mux
 }
--- main.go
+++ main.go
@@ -3,24 +3,32 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 	"log"
+	"log/slog"
+	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 	"github.com/leavedtrait/go-ota/cmd/api"
+	"github.com/leavedtrait/go-ota/internal/db"
 	"github.com/leavedtrait/go-ota/internal/server"
 )
 
 func main() {
-	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	
+	conn, err := db.ConnectToDB()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
+	slog.Info("DB connected")
 	defer conn.Close(context.Background())
 
 	router := server.NewRouter(conn)
+
 	srv := api.NewServer(":3000", router)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err.Error())

4725c4ca1ed9ccfe60a3e3fe84827dd4fc2828f3
leavedtrait <leavedtrait@gmail.com>
2025-04-16 19:35:47 +0300

    updated sql schema

--- internal/db/connect.go
+++ internal/db/connect.go
@@ -5,8 +5,10 @@ import (
 	"os"
 	"github.com/jackc/pgx/v5"
 )
+const schema = " CREATE TABLE IF NOT EXISTS users (id BIGSERIAL PRIMARY KEY,  email TEXT UNIQUE NOT NULL, name TEXT NOT NULL, password TEXT NOT NULL);"
 func ConnectToDB() (*pgx.Conn ,error){
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	conn.Exec(context.Background(), schema)
 	return conn,err
 }
